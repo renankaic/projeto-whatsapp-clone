15e09391ec4feee5da71fd9b6ba0bc77b7afeede
Renan Kaic Lopes <renan-kaic@outlook.com>
2020-01-20 11:28:24 -0300

    Feat - Taking a picture from the camera using the canvas HTML object

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -216,7 +216,29 @@ export default class WhatsAppController {
 
         this.el.btnTakePicture.on('click', e => {
 
-            console.log('Take picture')
+            let dataUrl = this._camera.takePicture()
+
+            this.el.pictureCamera.src = dataUrl
+            this.el.pictureCamera.show()
+            this.el.videoCamera.hide()
+            this.el.btnReshootPanelCamera.show()
+            this.el.containerTakePicture.hide()
+            this.el.containerSendPicture.show()
+            
+
+        })
+
+        this.el.btnReshootPanelCamera.on('click', e => {
+            this.el.pictureCamera.hide()
+            this.el.videoCamera.show()
+            this.el.btnReshootPanelCamera.hide()
+            this.el.containerTakePicture.show()
+            this.el.containerSendPicture.hide()
+        })
+
+        this.el.btnSendPicture.on('click', e => {
+
+            
 
         })
 
