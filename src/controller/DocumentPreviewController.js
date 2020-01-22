510304683fb3b44876f95ecdd287ad3acf7810f8
Renan Kaic Lopes <renan-kaic@outlook.com>
2020-01-22 14:27:10 -0300

    Feat - Adding support for PDF file preview when attaching this type of document

--- src/controller/DocumentPreviewController.js
+++ src/controller/DocumentPreviewController.js
@@ -1,3 +1,9 @@
+const pdfjsLib = require('pdfjs-dist')
+const path = require('path')
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = path.resolve(__dirname, '../../dist/pdf.worker.bundle.js')
+
+
 export class DocumentPreviewController {
 
     constructor(file) {
@@ -10,14 +16,14 @@ export class DocumentPreviewController {
 
         return new Promise((resolve, reject) => {
 
+            let reader = new FileReader()
+
             switch (this._file.type){
 
                 case 'image/png':
                 case 'image/jpeg':
                 case 'image/jpg':
                 case 'image/gif':
-                    let reader = new FileReader()
-
                     reader.onload = (e) => {
 
                         resolve({ 
@@ -33,6 +39,53 @@ export class DocumentPreviewController {
                 break
 
                 case 'application/pdf':
+
+                    reader.onload = e => {
+
+                        pdfjsLib.getDocument(new Uint8Array(reader.result))
+                            .then(pdf => {
+
+                                pdf.getPage(1)
+                                    .then(page => {
+
+                                        let viewport = page.getViewport(1)
+
+                                        let canvas = document.createElement('canvas')
+                                        let context = canvas.getContext('2d')
+
+                                        canvas.width = viewport.width
+                                        canvas.height = viewport.height
+
+                                        page.render({
+                                            canvasContext: context,
+                                            viewport
+                                        })
+                                        .then(() => {
+
+                                            let s = (pdf.numPages > 1) ? 's':''
+
+                                            resolve({
+                                                src: canvas.toDataURL('image/png'),
+                                                info: `${pdf.numPages} página${s}`
+                                            })
+                                            
+                                        })
+                                        .catch(err => {
+                                            reject(err)
+                                        })
+
+
+                                    })
+                                    .catch(err => {
+                                        reject(err)
+                                    })
+
+                            }).catch(err => {
+                                reject(err)
+                            })
+                    }
+
+                    reader.readAsArrayBuffer(this._file)
                 break
 
                 default:
--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -258,6 +258,10 @@ export default class WhatsAppController {
 
             if (this.el.inputDocument.files.length) {
 
+                this.el.panelDocumentPreview.css({
+                    'height': '1%'
+                })
+
                 let file = this.el.inputDocument.files[0]
 
                 this._documentPreviewController = new DocumentPreviewController(file)
@@ -269,7 +273,15 @@ export default class WhatsAppController {
                     this.el.imagePanelDocumentPreview.show()
                     this.el.filePanelDocumentPreview.hide()
 
+                    this.el.panelDocumentPreview.css({
+                        'height': 'calc(100% - 120px)'
+                    })
+
                 }).catch(err => {
+
+                    this.el.panelDocumentPreview.css({
+                        'height': 'calc(100% - 120px)'
+                    })
 
                     switch (file.type) {
 
