ba62e553609b1de87392165450a78131d8714bc9
Renan Kaic Lopes <renan-kaic@outlook.com>
2020-01-27 09:37:25 -0300

    Ref - Improving microphone timer behavior

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -347,7 +347,6 @@ export default class WhatsAppController {
 
             this.el.recordMicrophone.show()
             this.el.btnSendMicrophone.hide()
-            this.startRecordMicrophoneTime()
 
             this._microphoneController = new MicrophoneController()
             this._microphoneController.on('ready', audio => {
@@ -355,6 +354,12 @@ export default class WhatsAppController {
                 console.log(`Microphone ready`)
 
                 this._microphoneController.startRecorder()
+
+            })
+
+            this._microphoneController.on('recordtimer', timer => {
+
+                this.el.recordMicrophoneTimer.innerHTML = Format.toTime(timer)
 
             })
 
@@ -458,23 +463,10 @@ export default class WhatsAppController {
 
     }
 
-    startRecordMicrophoneTime() {
-
-        let start = Date.now()
-
-        this._recordMicrophoneInterval = setInterval(() => {
-            
-            this.el.recordMicrophoneTimer.innerHTML = Format.toTime((Date.now() - start))
-
-        }, 100)
-
-    }
-
     closeRecordMicrophone() {
 
         this.el.recordMicrophone.hide()
         this.el.btnSendMicrophone.show()
-        clearInterval(this._recordMicrophoneInterval)
 
     }
 
